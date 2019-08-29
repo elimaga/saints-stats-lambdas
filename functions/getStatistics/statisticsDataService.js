5caddf9bd0031dda78ee2434c5656db92642b9f2
Eli Magallanes <elimaga1996@gmail.com>
2019-08-28 22:27:25 -0700

    Mapping the rows of players and categories to one object for each player with all stats in it;

--- functions/getStatistics/statisticsDataService.js
+++ functions/getStatistics/statisticsDataService.js
@@ -1,14 +1,36 @@
 const dbServices = require('./dbServices');
 
 function getStatistics(callback) {
-    const getStatisticsQuery = 'SELECT P.Number, P.Name, SC.Abbreviation, SC.CategoryName, S.Value ' +
+    const getStatisticsQuery = 'SELECT P.Number, P.Name, SC.Abbreviation, S.Value ' +
                                'FROM Statistics S ' +
                                'INNER JOIN Players P on S.PlayerId = P.Id ' +
                                'INNER JOIN StatsCategories SC on S.CategoryId = SC.Id ' +
                                'ORDER BY P.Number, SC.Id';
     const getStatisticsArgs = [];
 
-    dbServices.query(getStatisticsQuery, getStatisticsArgs, callback);
+    dbServices.query(getStatisticsQuery, getStatisticsArgs, function (err, statistics) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        let statsForEachPlayer = [];
+
+        let playerStats = {};
+        statistics.forEach(statistic => {
+            if (statistic.Number === playerStats.Number) {
+                playerStats[statistic.Abbreviation] = statistic.Value;
+            } else {
+                playerStats = {};
+                statsForEachPlayer.push(playerStats);
+                playerStats.Number = statistic.Number;
+                playerStats.Name = statistic.Name;
+                playerStats[statistic.Abbreviation] = statistic.Value;
+            }
+        });
+
+        callback(null, statsForEachPlayer);
+    });
 }
 
 module.exports = {
--- test/getStatistics.test.js
+++ test/getStatistics.test.js
@@ -50,11 +50,11 @@ describe('getStatistics Lambda Test', () => {
         it('should query the database for the statistics', () => {
             getStatisticsLambda({}, {}, getStatisticsLambdaCallback);
 
-            const expectedDbQueryString = 'SELECT P.Number, P.Name, SC.Abbreviation, SC.CategoryName, S.Value ' +
-                                          'FROM Statistics S ' +
-                                          'INNER JOIN Players P on S.PlayerId = P.Id ' +
-                                          'INNER JOIN StatsCategories SC on S.CategoryId = SC.Id ' +
-                                          'ORDER BY P.Number, SC.Id';
+            const expectedDbQueryString = 'SELECT P.Number, P.Name, SC.Abbreviation, S.Value ' +
+                'FROM Statistics S ' +
+                'INNER JOIN Players P on S.PlayerId = P.Id ' +
+                'INNER JOIN StatsCategories SC on S.CategoryId = SC.Id ' +
+                'ORDER BY P.Number, SC.Id';
 
             const expectedDbArgs = [];
             assert.equal(dbMocks.dbConnectionMock.query.callCount, 1);
@@ -62,23 +62,38 @@ describe('getStatistics Lambda Test', () => {
             assert.equal(JSON.stringify(dbMocks.dbConnectionMock.query.args[0][1]), JSON.stringify(expectedDbArgs));
         });
 
-        it('should return the stats categories', () => {
+        it('should return the stats categories as an array of stats for each player', () => {
             getStatisticsLambda({}, {}, getStatisticsLambdaCallback);
 
             assert.equal(getStatisticsLambdaCallback.callCount, 0);
 
             const statisticsFake = [
-                {Number: 1, Name: 'Player1', Abbreviation: 'FC', CategoryName: 'Fake Category', Value: 8},
-                {Number: 1, Name: 'Player1', Abbreviation: 'CF', CategoryName: 'Category that is Fake', Value: 6},
-                {Number: 2, Name: 'Player2', Abbreviation: 'FC', CategoryName: 'Fake Category', Value: 5},
-                {Number: 2, Name: 'Player2', Abbreviation: 'CF', CategoryName: 'Category that is Fake', Value: 10}
+                {Number: 1, Name: 'Player1', Abbreviation: 'FC', Value: 8},
+                {Number: 1, Name: 'Player1', Abbreviation: 'CF', Value: 6},
+                {Number: 2, Name: 'Player2', Abbreviation: 'FC', Value: 5},
+                {Number: 2, Name: 'Player2', Abbreviation: 'CF', Value: 10}
             ];
             const getStatisticsFromDbCallback = dbMocks.dbConnectionMock.query.args[0][2];
             getStatisticsFromDbCallback(null, statisticsFake);
 
+            const expectedFormattedStats = [
+                {
+                    Number: 1,
+                    Name: 'Player1',
+                    FC: 8,
+                    CF: 6
+                },
+                {
+                    Number: 2,
+                    Name: 'Player2',
+                    FC: 5,
+                    CF: 10
+                }
+            ];
+
             assert.equal(getStatisticsLambdaCallback.callCount, 1);
             assert.equal(getStatisticsLambdaCallback.args[0][0], null);
-            assert.equal(getStatisticsLambdaCallback.args[0][1], statisticsFake);
+            assert.equal(JSON.stringify(getStatisticsLambdaCallback.args[0][1]), JSON.stringify(expectedFormattedStats));
             assert.equal(dbMocks.dbConnectionMock.end.callCount, 1);
         });
 
